634b95bb82d625cef0c781a8fcefefe57da0fa11
kjugk <k02ugk@gmail.com>
2016-01-15 13:08:29 +0900

    Install gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
 
 var browserify = require('browserify');
 var watchify = require('watchify');
@@ -9,24 +10,48 @@ var bebelify = require('babelify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var sourcemaps = require('gulp-sourcemaps');
-
-var b = watchify(browserify({
+var fs = require('fs');
+var glob = require('glob');
+var b = browserify({
   entries: ['./js/index.js'],
   transform: ['babelify'],
-  debug: true,
-}))
-.on('update', bundle)
-.on('log', gutil.log)
+})
 
-function bundle(){
+function clean(){
+  glob('./dist/*', {}, function(err, files){
+    files.forEach(function(file){
+      fs.unlink(file)
+    })
+  })
+}
+
+function productionBuild(){
   return b.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error')  )
     .pipe(source('bundle.js'))
     .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))
+    .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 }
 
-gulp.task('js', bundle);
-gulp.task('default', ['js']);
+function build(){
+  b = watchify(
+    b.on('update', bundle).on('log', gutil.log)
+  );
+  bundle()
+
+  function bundle(){
+    return b.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error')  )
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist'));
+  }
+}
+
+gulp.task('clean', clean)
+gulp.task('production', ['clean'], productionBuild);
+gulp.task('dev', ['clean'], build);
+gulp.task('default', ['dev']);
